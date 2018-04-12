2ef2300b299cbb9a90dcf47fbfb0cb82d88483b2
Kesley Alves <kesley0@hotmail.com>
2018-04-12 20:23:56 -0300

    modificações arquivo e teste

--- teste.java
+++ teste.java
@@ -2,7 +2,10 @@ public class teste{
 	public static void main(String[] args){
 		int num, n;
 		num= 7 + 3;
+		
+		n = 2 *  6; 
 		System.out.println("resultado: " + num);
+		System.out.println("multiplicação: " + n);
 		
 	}
 }
